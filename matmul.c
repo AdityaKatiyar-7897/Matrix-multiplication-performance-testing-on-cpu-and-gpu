0a5c2b262de902de4babcdd84265a738ffa02e4e
Aditya <rishkat1249@gmail.com>
2026-03-29 09:34:14 +0000

    Now this is real deal now we used all out cpu cores and results are amazing , it came out to be 1285.54 ms which is a 286x speedup comming from 368,638ms , now that's cool!

--- matmul.c
+++ matmul.c
@@ -1,8 +1,9 @@
-// here we multiplying big matrix but optimising it using blocking , i.e cutting it to fit into cache
+// here we multiplying big matrix but using all the cores of our cpu
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #define N 4096
 #define BLOCK 64
@@ -18,17 +19,20 @@ int main() {
             B[i][j] = 1.0f;
         }
 
-    clock_t start = clock();
-
-    for (int i = 0; i < N; i++)
-        for (int k = 0; k < N; k++)
-            for (int j = 0; j < N; j++)
-                C[i][j] += A[i][k] * B[k][j];
-
-    clock_t end = clock();
-
-    printf("N = %d, time = %.2f ms\n", N,
-           1000.0 * (end - start) / CLOCKS_PER_SEC);
+   double start = omp_get_wtime();
+           
+           #pragma omp parallel for
+           for (int ii = 0; ii < N; ii += BLOCK)
+               for (int kk = 0; kk < N; kk += BLOCK)
+                   for (int jj = 0; jj < N; jj += BLOCK)
+                       for (int i = ii; i < ii + BLOCK; i++)
+                           for (int k = kk; k < kk + BLOCK; k++)
+                               for (int j = jj; j < jj + BLOCK; j++)
+                                   C[i][j] += A[i][k] * B[k][j];
+           
+           double end = omp_get_wtime();
+           
+           printf("N = %d, time = %.2f ms\n", N, (end - start) * 1000.0);
 
     return 0;
 }
