239048dc68e2b0a926acdc448e76ab90800ac5b6
Aditya <rishkat1249@gmail.com>
2026-03-29 08:20:31 +0000

    THIS IS MATRIX MULTIPLICATION HAPPEING USING CPU ON A 256 X 256 MATRIX , the result we got is it tool :51.19ms: to run , pretty decent i have to say but now lets see what happens if we increase out value of N

---
+++ matmul.c
@@ -0,0 +1,33 @@
+// here we multiply two small matrices on you cpu 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 256
+
+float A[N][N];
+float B[N][N];
+float C[N][N];
+
+int main() {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            A[i][j] = 1.0f;
+            B[i][j] = 1.0f;
+        }
+
+    clock_t start = clock();
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            for (int k = 0; k < N; k++)
+                C[i][j] += A[i][k] * B[k][j];
+
+    clock_t end = clock();
+
+    printf("N = %d, time = %.2f ms\n", N,
+           1000.0 * (end - start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
