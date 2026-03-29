98f35c86f1df21c794b98383541193d0752b4c3e
Aditya <rishkat1249@gmail.com>
2026-03-29 09:20:46 +0000

    Strangely it took more time 146964.39 ms, maybe its becasue of more loops but changing the flag from-O1 to -O3 took the time to  9334.59 ms , decent enough lets see what else we can do

--- matmul.c
+++ matmul.c
@@ -1,10 +1,11 @@
-// here we multiply two small matrices on you cpu but we change out n from 256 to 1024
+// here we multiplying big matrix but optimising it using blocking , i.e cutting it to fit into cache
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define N 4096
+#define BLOCK 64
 
 float A[N][N];
 float B[N][N];
@@ -19,10 +20,13 @@ int main() {
 
     clock_t start = clock();
 
-    for (int i = 0; i < N; i++)
-        for (int k = 0; k < N; k++)
-            for (int j = 0; j < N; j++)
-                C[i][j] += A[i][k] * B[k][j];
+    for (int ii = 0; ii < N; ii += BLOCK)
+        for (int kk = 0; kk < N; kk += BLOCK)
+            for (int jj = 0; jj < N; jj += BLOCK)
+                for (int i = ii; i < ii + BLOCK; i++)
+                    for (int k = kk; k < kk + BLOCK; k++)
+                        for (int j = jj; j < jj + BLOCK; j++)
+                            C[i][j] += A[i][k] * B[k][j];
 
     clock_t end = clock();
 
