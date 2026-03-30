93ff25c17492f0c54ac93b07b1385b96ba917b68
Aditya <rishkat1249@gmail.com>
2026-03-30 07:03:32 +0000

    Now we are talking , we saw a wierd behaviour , threads 32 ran first the then it when chronologically from 0 to 31 and thats strange , lets observe this behavious with various values like this , values that are offsetting from the warp size

--- learn_cuda.cu
+++ learn_cuda.cu
@@ -9,7 +9,7 @@ __global__ void who_am_i(){
 }
 
 int main(){
-	who_am_i<<<1, 64>>>();// to define blocks and threads
+	who_am_i<<<1, 33>>>();// to define blocks and threads
 	cudaDeviceSynchronize();
 	return 0;
 }
@@ -17,5 +17,37 @@ int main(){
 //output
 
 /*
-
+block 0, thread 32, global id = 32
+block 0, thread 0, global id = 0
+block 0, thread 1, global id = 1
+block 0, thread 2, global id = 2
+block 0, thread 3, global id = 3
+block 0, thread 4, global id = 4
+block 0, thread 5, global id = 5
+block 0, thread 6, global id = 6
+block 0, thread 7, global id = 7
+block 0, thread 8, global id = 8
+block 0, thread 9, global id = 9
+block 0, thread 10, global id = 10
+block 0, thread 11, global id = 11
+block 0, thread 12, global id = 12
+block 0, thread 13, global id = 13
+block 0, thread 14, global id = 14
+block 0, thread 15, global id = 15
+block 0, thread 16, global id = 16
+block 0, thread 17, global id = 17
+block 0, thread 18, global id = 18
+block 0, thread 19, global id = 19
+block 0, thread 20, global id = 20
+block 0, thread 21, global id = 21
+block 0, thread 22, global id = 22
+block 0, thread 23, global id = 23
+block 0, thread 24, global id = 24
+block 0, thread 25, global id = 25
+block 0, thread 26, global id = 26
+block 0, thread 27, global id = 27
+block 0, thread 28, global id = 28
+block 0, thread 29, global id = 29
+block 0, thread 30, global id = 30
+block 0, thread 31, global id = 31
 */
