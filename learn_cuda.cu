286f1038285887688e37f9298bcd05582e9e1b4e
Aditya <rishkat1249@gmail.com>
2026-03-30 06:59:50 +0000

    Okay nothing dramatic it runs in clean cronological order for 64 threads but , it was a perfect lot of 32 each lets see what happens when its not , lets try 33 this time and see what happens

--- learn_cuda.cu
+++ learn_cuda.cu
@@ -9,7 +9,7 @@ __global__ void who_am_i(){
 }
 
 int main(){
-	who_am_i<<<1, 32>>>();// to define blocks and threads
+	who_am_i<<<1, 64>>>();// to define blocks and threads
 	cudaDeviceSynchronize();
 	return 0;
 }
@@ -17,22 +17,5 @@ int main(){
 //output
 
 /*
-block 0, thread 0, global id = 0
-block 0, thread 1, global id = 1
-block 0, thread 2, global id = 2
-block 0, thread 3, global id = 3
-block 0, thread 4, global id = 4
-block 0, thread 5, global id = 5
-block 0, thread 6, global id = 6
-block 0, thread 7, global id = 7
-block 0, thread 8, global id = 8
-block 0, thread 9, global id = 9
-block 0, thread 10, global id = 10
-block 0, thread 11, global id = 11
-block 0, thread 12, global id = 12
-block 0, thread 13, global id = 13
-block 0, thread 14, global id = 14
-block 0, thread 15, global id = 15
 
-16 threads ran on 1 block , now lets try 32
 */
